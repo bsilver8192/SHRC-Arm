d47a876af36c9bd586cdef83e72fb541eddfe953
Brian <brian@localhost>
2014-09-14 15:53:09 -0400

    initial commit of an empty arm controller

---
+++ shrc/control_loops/python/arm.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import control_loop
+import numpy
+import sys
+from matplotlib import pylab
+
+class Arm(control_loop.ControlLoop):
+  def __init__(self, name="Arm"):
+    super(Arm, self).__init__(name)
